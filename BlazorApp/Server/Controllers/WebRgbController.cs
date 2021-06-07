2f2308e513f423433ca87881400ed5000ce6e22c
MrPresident <marcello.chirri@gmail.com>
2021-06-07 20:08:48 +0200

    First commit

---
+++ BlazorApp/Server/Controllers/WebRgbController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebRGB.Service;
+
+namespace BlazorApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebRgbController : Controller
+    {
+        [HttpGet]
+        public void Get()
+        {
+            var rgbSerice = new RGBService(23);
+            rgbSerice.Test();
+        }
+    }
+}
---
+++ WebRGB.Service/RGBService.cs
@@ -0,0 +1,33 @@
+﻿using OpenRGB.NET;
+using OpenRGB.NET.Models;
+using System;
+
+namespace WebRGB.Service
+{
+    public class RGBService
+    {
+        private int Leds;
+        public RGBService(int leds)
+        {
+            Leds = leds;
+        }
+        public void Test()
+        {
+            using OpenRGBClient client = new OpenRGBClient(name: "Ao");
+            var controllers = client.GetControllerCount();
+            var colors = new Color[Leds];
+            var rand = new Random();
+            for (int i = 0; i < Leds; i++)
+            {
+                var r = rand.Next(255);
+                var g = rand.Next(255);
+                var b = rand.Next(255);
+                colors[i] = new Color((byte)r, (byte)g, (byte)b);
+            }
+            for (int i = 0; i < controllers; i++)
+            {
+                client.UpdateLeds(i, colors);
+            }
+        }
+    }
+}
